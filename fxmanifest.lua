8938f619991d773fae68d06938bb79980981c563
Ghost Developments <davidmoore019@gmail.com>
2026-07-09 16:24:57 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,8 @@ author 'Ghost Developments'
 description 'Drug Selling converted to Qbox'
 version '1.0.0'
 
+shared_script '@ox_lib/init.lua'
+
 shared_script 'config.lua'
 
 client_script 'client.lua'
